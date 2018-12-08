2798d70a6e15c8a1362621fb7de9d307e1ca9d4c
WIS <38274782+WIS8@users.noreply.github.com>
2018-12-08 19:44:31 +0800

    11

---
+++ she/src/team/ecust/she/view/MessagesList.java
@@ -0,0 +1,16 @@
+package team.ecust.she.view;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MessagesList extends JPanel {
+	
+	public MessagesList() {
+
+	}
+	
+	public void display() {
+		int width = getWidth();
+		int height = getHeight();
+	}
+}
